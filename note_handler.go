17ed6ed32fc2209eb330e5b3d4ac427a4d4faf62
xwzQmxx <1499273991@qq.com>
2021-10-27 17:20:31 +0800

    complete plugin codding

---
+++ note_handler.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"strings"
+
+	"gitee.com/openeuler/go-gitee/gitee"
+)
+
+type noteHandler interface {
+	addLabel(label []string) error
+	addComment(comment string) error
+	removeLabel(label string) error
+	getLabels() (map[string]string, error)
+}
+
+type issueNoteHandle struct {
+	client iClient
+	org    string
+	repo   string
+	number string
+}
+
+func (inh *issueNoteHandle) addLabel(label []string) error {
+	return inh.client.AddMultiIssueLabel(inh.org, inh.repo, inh.number, label)
+}
+
+func (inh *issueNoteHandle) addComment(comment string) error {
+	return inh.client.CreateIssueComment(inh.org, inh.repo, inh.number, comment)
+}
+
+func (inh *issueNoteHandle) removeLabel(label string) error {
+	return inh.client.RemoveIssueLabel(inh.org, inh.repo, inh.number, label)
+}
+
+func (inh *issueNoteHandle) getLabels() (map[string]string, error) {
+	labels, err := inh.client.GetIssueLabels(inh.org, inh.repo, inh.number)
+	if err != nil {
+		return nil, err
+	}
+	return labelsTransformMap(labels), nil
+}
+
+type prNoteHandle struct {
+	client iClient
+	org    string
+	repo   string
+	number int32
+}
+
+func (pnh *prNoteHandle) addLabel(label []string) error {
+	return pnh.client.AddMultiPRLabel(pnh.org, pnh.repo, pnh.number, label)
+}
+
+func (pnh *prNoteHandle) addComment(comment string) error {
+	return pnh.client.CreatePRComment(pnh.org, pnh.repo, pnh.number, comment)
+}
+
+func (pnh *prNoteHandle) removeLabel(label string) error {
+	return pnh.client.RemovePRLabel(pnh.org, pnh.repo, pnh.number, label)
+}
+
+func (pnh *prNoteHandle) getLabels() (map[string]string, error) {
+	labels, err := pnh.client.GetPRLabels(pnh.org, pnh.repo, pnh.number)
+	if err != nil {
+		return nil, err
+	}
+	return labelsTransformMap(labels), nil
+}
+
+func labelsTransformMap(labels []gitee.Label) map[string]string {
+	lm := make(map[string]string, len(labels))
+	for _, v := range labels {
+		k := strings.ToLower(v.Name)
+		lm[k] = v.Name
+	}
+	return lm
+}
--- robot.go
+++ robot.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 
 	sdk "gitee.com/openeuler/go-gitee/gitee"
 	libconfig "github.com/opensourceways/community-robot-lib/config"
+	"github.com/opensourceways/community-robot-lib/giteeclient"
 	libplugin "github.com/opensourceways/community-robot-lib/giteeplugin"
 	"github.com/sirupsen/logrus"
 )
 
-// TODO: set botName
-const botName = ""
+const botName = "label"
 
 type iClient interface {
+	GetRepoLabels(owner, repo string) ([]sdk.Label, error)
+	GetIssueLabels(org, repo, number string) ([]sdk.Label, error)
+	GetPRLabels(org, repo string, number int32) ([]sdk.Label, error)
+
+	AddIssueLabel(org, repo, number, label string) error
+	RemoveIssueLabel(org, repo, number, label string) error
+
+	AddMultiIssueLabel(org, repo, number string, label []string) error
+	AddMultiPRLabel(org, repo string, number int32, label []string) error
+	RemovePRLabel(org, repo string, number int32, label string) error
+
+	CreatePRComment(org, repo string, number int32, comment string) error
+	CreateIssueComment(org, repo string, number string, comment string) error
+	ListPROperationLogs(org, repo string, number int32) ([]sdk.OperateLog, error)
+
+	IsCollaborator(owner, repo, login string) (bool, error)
 }
 
 func newRobot(cli iClient) *robot {
@@ -31,32 +47,68 @@ func (bot *robot) getConfig(cfg libconfig.PluginConfig) (*configuration, error) 
 	if c, ok := cfg.(*configuration); ok {
 		return c, nil
 	}
-	return nil, errors.New("can't convert to configuration")
+	return nil, fmt.Errorf("can't convert to configuration")
 }
 
 func (bot *robot) RegisterEventHandler(p libplugin.HandlerRegitster) {
-	p.RegisterIssueHandler(bot.handleIssueEvent)
 	p.RegisterPullRequestHandler(bot.handlePREvent)
 	p.RegisterNoteEventHandler(bot.handleNoteEvent)
-	p.RegisterPushEventHandler(bot.handlePushEvent)
 }
 
 func (bot *robot) handlePREvent(e *sdk.PullRequestEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand PR event, delete this function.
-	return nil
-}
+	prInfo := giteeclient.GetPRInfoByPREvent(e)
 
-func (bot *robot) handleIssueEvent(e *sdk.IssueEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand Issue event, delete this function.
-	return nil
-}
+	cfgForRepo := bot.getConfigItem(prInfo.Org, prInfo.Repo, cfg, log)
+	if cfgForRepo == nil {
+		return nil
+	}
+	prHandle := &prNoteHandle{client: bot.cli, org: prInfo.Org, repo: prInfo.Repo, number: prInfo.Number}
 
-func (bot *robot) handlePushEvent(e *sdk.PushEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand Push event, delete this function.
+	switch giteeclient.GetPullRequestAction(e) {
+	case giteeclient.PRActionChangedSourceBranch:
+		return bot.handleClearLabel(prHandle, cfgForRepo)
+	case "edited":
+		return bot.handleValidatingLabel(prHandle, prInfo, cfgForRepo, log)
+	}
+
 	return nil
 }
 
 func (bot *robot) handleNoteEvent(e *sdk.NoteEvent, cfg libconfig.PluginConfig, log *logrus.Entry) error {
-	// TODO: if it doesn't needd to hand Note event, delete this function.
-	return nil
+	ne := giteeclient.NewNoteEventWrapper(e)
+	if !ne.IsCreatingCommentEvent() {
+		log.Debug("Event is not a creation of a comment, skipping.")
+		return nil
+	}
+
+	matchLabels := genMachLabels(ne.GetComment())
+	if matchLabels == nil {
+		log.Debug("comment content needn't handle, skipping.")
+		return nil
+	}
+
+	return bot.handleLabels(ne, matchLabels, cfg, log)
+}
+
+func (bot *robot) getConfigItem(org, repo string, cfg libconfig.PluginConfig, log *logrus.Entry) *botConfig {
+	config, err := bot.getConfig(cfg)
+	if err != nil {
+		return nil
+	}
+
+	cfgItem := config.configFor(org, repo)
+	if cfgItem == nil {
+		log.Info(fmt.Sprintf("no %s robot config for this repo:%s/%s", botName, org, repo))
+		return nil
+	}
+
+	return cfgItem
+}
+
+func (bot *robot) getRepoLabelsMap(org, repo string) (map[string]string, error) {
+	repoLabels, err := bot.cli.GetRepoLabels(org, repo)
+	if err != nil {
+		return nil, err
+	}
+	return labelsTransformMap(repoLabels), nil
 }
